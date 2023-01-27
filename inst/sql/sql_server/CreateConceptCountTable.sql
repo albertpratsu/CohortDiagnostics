3156273609d396f0362e281aaabdb165f17cdc83
albertpratsu <44193724+albertpratsu@users.noreply.github.com>
2023-01-27 11:16:46 +0000

    Update CreateConceptCountTable.sql

--- inst/sql/sql_server/CreateConceptCountTable.sql
+++ inst/sql/sql_server/CreateConceptCountTable.sql
@@ -9,17 +9,18 @@ IF OBJECT_ID('@work_database_schema.@concept_counts_table', 'U') IS NOT NULL
 } 
 
 IF OBJECT_ID('results.concept_counts_perm4', 'U') IS NOT NULL  
-     { SELECT concept_id,
+	SELECT concept_id,
 	concept_count,
 	concept_subjects
-{@table_is_temp} ? {
-INTO @concept_counts_table
-} : { 
-INTO @work_database_schema.@concept_counts_table
-}
-FROM results.concept_counts_perm4; }   
+	{@table_is_temp} ? {
+	INTO @concept_counts_table
+	} : { 
+	INTO @work_database_schema.@concept_counts_table
+	}
+	FROM results.concept_counts_perm4 ;
+
  ELSE   
-     { SELECT concept_id,
+    SELECT concept_id,
 	concept_count,
 	concept_subjects
 {@table_is_temp} ? {
@@ -105,4 +106,4 @@ FROM (
 		COUNT_BIG(DISTINCT person_id) AS concept_subjects
 	FROM @cdm_database_schema.observation
 	GROUP BY observation_source_concept_id
-	) tmp; }
+	) tmp;
