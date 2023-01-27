90fdbb5d12366537b8ff9a514d1b37c9d814fb90
albertpratsu <44193724+albertpratsu@users.noreply.github.com>
2023-01-27 12:16:02 +0000

    Update CreateConceptCountTable.sql

--- inst/sql/sql_server/CreateConceptCountTable.sql
+++ inst/sql/sql_server/CreateConceptCountTable.sql
@@ -8,12 +8,13 @@ IF OBJECT_ID('@work_database_schema.@concept_counts_table', 'U') IS NOT NULL
 	DROP TABLE @work_database_schema.@concept_counts_table;
 }
 
-SELECT concept_id,
-	concept_count,
-	concept_subjects
-{@table_is_temp} ? {
-INTO @concept_counts_table
-} : { 
-INTO @work_database_schema.@concept_counts_table
-}
-FROM results.concept_counts_perm4 ;
+IF OBJECT_ID('results.concept_counts_perm4', 'U') IS NOT NULL
+	SELECT concept_id,
+		concept_count,
+		concept_subjects
+	{@table_is_temp} ? {
+	INTO @concept_counts_table
+	} : { 
+	INTO @work_database_schema.@concept_counts_table
+	}
+	FROM results.concept_counts_perm4 ;
