046f55cfe888846d65a11ab84a15cb26b95a1429
Abert P <albert.prats-Uribe@ndorms.ox.ac.uk>
2023-02-14 15:01:59 +0000

    commit function without documentation

---
+++ R/CreateCountsTable.R
@@ -0,0 +1,61 @@
+# Copyright 2023 OXINFER
+#
+# This file is part of CohortDiagnostics
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+#' @param connection Connection to tha database created with DatabaseConnector
+#' @param cdmDatabaseSchema Schema where cdm is stored
+#' @param writeDatabaseSchema Schema where table should be created
+#' 
+#' @export
+#' 
+#' @return 
+#' @example 
+#' 
+createCountsTable <- function(connection,
+                              cdmDatabaseSchema,
+                              writeDatabaseSchema) {
+  sql <-
+    SqlRender::loadRenderTranslateSql(
+      "CreationOfConceptCountTable.sql",
+      packageName = utils::packageName(),
+      dbms = connection@dbms,
+      cdm_database_schema = cdmDatabaseSchema,
+      work_database_schema = writeDatabaseSchema
+    )
+  DatabaseConnector::executeSql(connection, sql)
+}
+
+#' @param connection Connection to tha database created with DatabaseConnector
+#' @param cdmDatabaseSchema Schema where cdm is stored
+#' @param writeDatabaseSchema Schema where table should be created
+#' 
+#' @noRd
+#' 
+#' @return 
+#' @example 
+#' 
+createCountsTableSample <- function(connection,
+                                    cdmDatabaseSchema,
+                                    writeDatabaseSchema) {
+  sql <-
+    SqlRender::loadRenderTranslateSql(
+      "CreationOfConceptCountTableL100.sql",
+      packageName = utils::packageName(),
+      dbms = connection@dbms,
+      cdm_database_schema = cdmDatabaseSchema,
+      work_database_schema = writeDatabaseSchema
+    )
+  DatabaseConnector::executeSql(connection, sql)
+}
--- inst/sql/sql_server/CreationOfConceptCountTable.sql
+++ inst/sql/sql_server/CreationOfConceptCountTable.sql
@@ -1,11 +1,11 @@
 {DEFAULT @table_is_temp = FALSE}
 
 {@table_is_temp} ? {
-IF OBJECT_ID('tempdb..@concept_counts_table', 'U') IS NOT NULL
-  DROP TABLE @concept_counts_table;
+IF OBJECT_ID('tempdb..cohort_diagnostics_concept_counts_permanent_table', 'U') IS NOT NULL
+  DROP TABLE cohort_diagnostics_concept_counts_permanent_table;
 } : {
-IF OBJECT_ID('@work_database_schema.@concept_counts_table', 'U') IS NOT NULL
-	DROP TABLE @work_database_schema.@concept_counts_table;
+IF OBJECT_ID('@work_database_schema.cohort_diagnostics_concept_counts_permanent_table', 'U') IS NOT NULL
+	DROP TABLE @work_database_schema.cohort_diagnostics_concept_counts_permanent_table;
 }
 
 SELECT concept_id,
